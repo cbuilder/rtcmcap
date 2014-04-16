1c3fe8b944d94fbb59a36699ab5f087ba56c8de6
Dima Alekseev <alexeev6@yahoo.com>
2014-04-16 09:19:11 +0400

    Added some port configuration

--- rtcmcap.c
+++ rtcmcap.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <termios.h>
 
 #define BAUDRATE 19200
 #define RTCMDEVICE "/dev/ttyS2"
 
-char buf[255];
+struct termios term_settings, orig_term_settings;
 
 int rcvr_port_open() {
 	int fd,c, res;
@@ -15,11 +16,27 @@ int rcvr_port_open() {
 		perror(RTCMDEVICE);
 		exit(-1);
 	}
+	tcgetattr(fd, &orig_term_settings);
+	memset(&term_settings, 0, sizeof(term_settings));
+	term_settings.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	term_settings.c_iflag = IGNPAR;
+	term_settings.c_oflag = 0;
+	term_settings.c_lflag = 0;
+	
+	term_settings.c_cc[VTIME] = 0;
+	term_settings.c_cc[VMIN] = 1;
+
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd, TCSANOW, &term_settings);
 	return fd;
 }
 
 int main()
 {
-	int rtcm_fd = rcvr_port_open();
+	char input;
+	int rtcm_stream_fd = rcvr_port_open();
+	while (1) {
+		read(rtcm_stream_fd, &input, 1);
+	}
 	return 0;
 }
