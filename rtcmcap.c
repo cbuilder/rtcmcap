6b06b1d8caac6074b58ecb0603ed36d71ce536d4
Dima Alekseev <alexeev6@yahoo.com>
2014-04-18 10:15:57 +0400

    Learned to find start of message, able to receive messages with preamble 0x59

--- rtcmcap.c
+++ rtcmcap.c
@@ -8,11 +8,14 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#define RATE			19200
-#define BRATE			B##19200
-#define RTCMDEVICE		"/dev/ttyS2"
+#define DEBUG
+#define RATE		19200
+#define BRATE		B##19200
+#define RTCMDEVICE	"/dev/ttyS2"
 #define RTCM2_PREAMBLE	0x66
+#define UNKNOWN_PREAMBLE	0x59
 #define FRAME_BUF_SZ	256
+#define MAX_T_BETW_BYTES_IN_MSG (1/RATE * 3)*1000000 /* usec */
 
 volatile unsigned char stop = 0;
 struct termios term_settings, orig_term_settings;
@@ -52,6 +55,11 @@ void exit_handler()
     stop = 1;
 }
 
+void line_activity_timeout_handler()
+{
+    puts("The receiver is keeping silence");
+}
+
 void msg_rcv_timeout_handler()
 {
     ;
@@ -59,9 +67,14 @@ void msg_rcv_timeout_handler()
 
 unsigned char * receive(unsigned char *buf, int fd)
 {
+    struct itimerval curr_iti;
+    const struct itimerval pream_rcv_iti = {
+        .it_interval = { 0, 0 },
+        .it_value = {2, 0}, /* Max time between messages */
+    };
     const struct itimerval msg_rcv_iti = {
         .it_interval = { 0, 0 },
-        .it_value = {0, 100}, /* 1 ms */
+        .it_value = {0, 500},
     };
     const struct itimerval disable_iti = {
         .it_interval = { 0, 0 },
@@ -70,9 +83,26 @@ unsigned char * receive(unsigned char *buf, int fd)
     unsigned int rcvd_msg_len = 0;
     unsigned int nread = 0;
     memset(buf, 0, sizeof(buf));
+
+    if (signal(SIGALRM, line_activity_timeout_handler) == SIG_ERR)
+        perror("signal to wait preamble");
+    if (setitimer(ITIMER_REAL, &pream_rcv_iti, NULL) == -1)
+        perror("setitimer on receive preamble");
     nread = read(fd, &buf[0], 1);
-    if (nread > 0) {
-        if (buf[0] == RTCM2_PREAMBLE) {
+    getitimer(ITIMER_REAL, &curr_iti);
+    unsigned long usec_elapsed_from_pream_rcv =
+            ((unsigned long)pream_rcv_iti.it_value.tv_sec * 1000000
+            + (unsigned long)pream_rcv_iti.it_value.tv_usec)
+            - ((unsigned long)curr_iti.it_value.tv_sec * 1000000
+            + (unsigned long)curr_iti.it_value.tv_usec);
+#ifdef DEBUG
+    printf("usec elapsed from last msg: %ld\n", usec_elapsed_from_pream_rcv);
+#endif
+    if (setitimer(ITIMER_REAL, &disable_iti, NULL) == -1)
+        perror("setitimer to stop");
+
+    if (nread > 0 && usec_elapsed_from_pream_rcv > 500) {
+        if (buf[0] == RTCM2_PREAMBLE || buf[0] == UNKNOWN_PREAMBLE) {
             unsigned int i = 1;
             while (1) {
                 if (signal(SIGALRM, msg_rcv_timeout_handler) == SIG_ERR)
@@ -95,12 +125,14 @@ unsigned char * receive(unsigned char *buf, int fd)
                 }
             }
             rcvd_msg_len = i;
+#ifdef DEBUG
             printf("Received Message:");
             for (unsigned j = 0; j < rcvd_msg_len; j++) {
                 if (j > 0) printf(",");
                 printf("%02X", buf[j]);
             }
             printf("\nLength: %u\n",rcvd_msg_len);
+#endif
         }
     } else if (nread == 0) {
         printf("Receiver on %s is not alive", RTCMDEVICE);
