b612f83e2e2ac715585c4a562ae00e632a3b64aa
Dima Alekseev <alexeev6@yahoo.com>
2014-04-21 13:21:30 +0400

    Current project definitions header. Forgotten.

---
+++ rtcmcap.h
@@ -0,0 +1,13 @@
+/*
+ * File: rcmcap.h
+ * Description: Main project definitions 
+ */
+
+#define RATE		19200
+#define BRATE		B##19200
+#define RTCMDEVICE	"/dev/ttyS2"
+#define RTCM2_PREAMBLE	0x66
+#define UNKNOWN_PREAMBLE	0x59
+#define FRAME_BUF_SZ	256
+#define CLIENT_IPADDR "192.168.2.1"
+#define CLIENT_PORT 8998
