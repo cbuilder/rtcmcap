6a40abb7243985e6f1615a34a8cbda1cb9e4b65c
Dima Alekseev <alexeev6@yahoo.com>
2014-05-10 12:45:30 +0400

    Correct mistake with double definition

--- sender.h
+++ sender.h
@@ -3,13 +3,6 @@
  * Description: Network function prototypes
  */
 
-#ifndef NI_MAXHOST
-#define NI_MAXHOST    1025
-#endif
-#ifndef NI_MAXSERV
-#define NI_MAXSERV      32
-#endif
-
 int sock_create();
 void sock_close();
 int send_within_protobuf(int socket_fd, unsigned char *buf, size_t num_bytes);
